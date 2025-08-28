5e0ad881876bfb26e3d0ab20b6c72c7423b35054
Priyansh Kayastha <priyanshkayastha0803@gmail.com>
2025-08-28 22:06:19 +0530

    Ascii

---
+++ logics/Acscii.java
@@ -0,0 +1,13 @@
+ package logics;
+
+public class Acscii {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        int Ascii = ch; 
+
+        System.out.println("ASCII value of '" + ch + "' is: " + Ascii);
+    }
+} Ascii {
+    
+}
